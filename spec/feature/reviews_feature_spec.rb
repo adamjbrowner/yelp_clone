b18935787a6742dfdb77ab18bf4ee6438ede7b9d
Adam <adamjbrowner@gmail.com>
2016-08-26 11:32:12 +0100

    Added average rating stars

--- spec/feature/reviews_feature_spec.rb
+++ spec/feature/reviews_feature_spec.rb
@@ -13,4 +13,13 @@ feature 'reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
+
+  scenario 'display an average rating for all reviews' do
+    sign_up
+    leave_review("Okay", '3')
+    click_link "Sign out"
+    sign_up("test2@email.com", "2938472")
+    leave_review("Brilliant", '5')
+    expect(page).to have_content('Average rating: ★★★★☆')
+  end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,8 +1,16 @@
-def sign_up
+def sign_up(email = "test2@test.com", password = "1234567")
   visit '/'
   click_link 'Sign up'
-  fill_in 'Email', with: "test@test.com"
-  fill_in 'Password', with: "password123"
-  fill_in 'Password confirmation', with: "password123"
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password
   click_button 'Sign up'
+end
+
+def leave_review(thoughts, rating)
+  visit '/restaurants'
+  click_link "Review KFC"
+  fill_in 'Thoughts', with: thoughts
+  select rating, from: 'Rating'
+  click_button 'Leave Review'
 end
