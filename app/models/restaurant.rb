1906f9e6b5d5fb6f5c19bdce3a720c13ee9e43c4
BartJudge <BartJudge@gmail.com>
2016-08-26 10:24:32 +0100

    yelp extending associations via modules

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,15 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
+  has_many :reviews,
+        -> { extending WithUserAssociationExtension },
+        dependent: :destroy
+
+
+  def build_review(attributes = {}, user)
+    review = reviews.build(attributes)
+    review.user = user
+    review
+  end
 
 end
